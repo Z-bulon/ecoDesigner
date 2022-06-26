24f162c84963271d5d6d57823a4bad04e0525ca0
Arliack <30591667+Arliack@users.noreply.github.com>
2022-06-26 06:23:33 +0200

    Initial Commit

---
+++ Shared/ecoDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoDesignerApp.swift
+//  Shared
+//
+//  Created by Jeremy Pagerit on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ecoDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
